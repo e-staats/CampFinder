35d1f20c22f65c0a480701fd7e10083413575b5f
Eric Staats <eric.k.staats@gmail.com>
2020-12-16 22:12:07 -0600

    added alembic for db migrations

--- data/all_tables.py
+++ data/all_tables.py
@@ -4,3 +4,4 @@ from data.availability import Availability # pylint: disable = import-error
 from data.region import Region # pylint: disable = import-error
 from data.user import User # pylint: disable = import-error
 from data.park import Park # pylint: disable = import-error
+from data.security import Security # pylint: disable = import-error
--- data/user.py
+++ data/user.py
@@ -9,10 +9,13 @@ class User(SqlAlchemyBase):
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     name = sa.Column(sa.String)
     email = sa.Column(sa.String, index=True, unique=True)
+    phone_number = sa.Column(sa.String)
     hashed_pw = sa.Column(sa.String)
     creation_date = sa.Column(sa.DateTime, default=datetime.datetime.now)
     is_active = sa.Column(sa.Integer, default=True)
     security_class = sa.Column(sa.Integer, ForeignKey("security.id"))
+    send_emails = sa.Column(sa.Integer)
+    send_texts = sa.Column(sa.Integer)
     
     def __repr__(self):
         return f"User {self.name}"
